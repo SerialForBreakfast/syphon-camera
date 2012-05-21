60100ed61397f19f313cde0a4ee9cc968c2a8dcf
normen667 <normen667@4d62856e-876d-231e-676b-023126e86255>
2012-05-21 15:58:35 +0000

    adding initial versions

---
+++ trunk/SyphonNetCamera/SyphonNetCamera/SyphonSender.h
@@ -0,0 +1,48 @@
+//
+//  SyphonSender.h
+//  SyphonNetCamera
+//
+//  Created by Normen Hansen on 17.05.12.
+//  Copyright (c) 2012 Normen Hansen. Released under New BSD license.
+//
+
+#import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
+#import <OpenGL/OpenGL.h>
+#import <OpenGL/gl.h>
+#import <OpenGL/glext.h>
+#import <Syphon/Syphon.h>
+#import "NetCamClient.h"
+
+@interface SyphonSender : NSObject<NetCamClientDelegate>{
+    NSOpenGLContext *glContext;
+    SyphonServer *server;
+    NetCamClient *client;
+    
+    GLuint framebuffer, renderbuffer;
+    GLuint texture;
+    void *imageData;
+    NSInteger curWidth;
+    NSInteger curHeight;
+    
+    NSThread *readThread;
+    
+    tjhandle jpeg;
+
+    BOOL finished;
+}
+
+- (id)initWithData:(NSDictionary *)dict;
+- (NSDictionary *)getData;
+
+- (void) netCamClient:(NetCamClient*) client didReceiveImage:(NSData*) imagedata;
+- (void) netCamClient:(NetCamClient*) client didReceiveError:(NSError*) error;
+- (void) cleanup;
+
+@property (nonatomic, assign) NSString *url;
+@property (nonatomic, assign) NSString *user;
+@property (nonatomic, assign) NSString *password;
+@property (nonatomic, assign) NSString *name;
+@property (nonatomic) BOOL enabled;
+
+@end
